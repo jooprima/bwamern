8345a38491be06714393ccf024108b82a6d627c4
jooprima <jooprima@users-MacBook-Pro.local>
2022-01-22 22:31:08 +0700

    setup scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
+import React from "react";
+import "assets/scss/style.scss";
 
 function App() {
-  return (
-    <div className="App">
-      haiii jooo!
-    </div>
-  );
+  return <div className="App">haiii jooo!</div>;
 }
 
 export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import second from 'elements';
+;
+
+
+export default class LandingPage extends Component {
+  render() {
+    return <div></div>;
+  }
+}
