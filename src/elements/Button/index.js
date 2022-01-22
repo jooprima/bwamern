7df9d6c206a945fbc347c63c4451b25eede9c484
jooprima <jooprima@users-MacBook-Pro.local>
2022-01-22 23:40:39 +0700

    component button part2

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -4,12 +4,51 @@ import { Link } from "react-router-dom";
 import propTypes from "prop-types";
 
 export default function Button(props) {
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
+
+  const onclick = () => {
+    if (props.onclick) props.onclick();
+  };
+
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      return (
+        <Link
+          to={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          onClick={onClick}
+        >
+          {props.children}
+        </Link>
+      );
+    }
+  }
+
   return <div></div>;
 }
 
 Button.propTypes = {
   type: propTypes.oneOf(["button", "link"]),
   onclick: propTypes.func,
+  href: propTypes.string,
   target: propTypes.string,
   className: propTypes.string,
   isDisabled: propTypes.bool,
@@ -19,6 +58,3 @@ Button.propTypes = {
   isBlock: propTypes.bool,
   hasShadow: propTypes.bool,
 };
-
-// ;sdfdsaf
-
