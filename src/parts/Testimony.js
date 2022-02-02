c04be3efe3b4436b296ed1b7a14152bf91148643
jooprima <jooprima@gmail.com>
2022-02-02 21:17:06 +0700

    component testimony part2

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Star from "elements/Star";
 import TestimonyAccent from "assets/images/testimonial-landingpages-frame.jpg";
+import Button from "elements/Button";
 
 export default function Testimony({ data }) {
   return (
@@ -28,6 +29,22 @@ export default function Testimony({ data }) {
           <h5 className="h2 font-weight-light line-height-2 my-3">
             {data.content}
           </h5>
+          <span className="text-gray-500">
+            {data.familyName}, {data.familyOccupation}
+          </span>
+
+          <div>
+            <Button
+              className="btn px-5"
+              style={{ marginTop: 40 }}
+              hasShadow
+              isPrimary
+              type="link"
+              href={`/testimonial/${data._id}`}
+            >
+              Read Their Story
+            </Button>
+          </div>
         </div>
       </div>
     </div>
