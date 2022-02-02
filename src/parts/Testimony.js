6d586ee9e265fc2a918e229cc134f730676d3de2
jooprima <jooprima@gmail.com>
2022-02-02 21:00:59 +0700

    component testimony part1

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -1,10 +1,35 @@
-import Star from "elements/Star";
 import React from "react";
+import Star from "elements/Star";
+import TestimonyAccent from "assets/images/testimonial-landingpages-frame.jpg";
 
-export default function Testimony() {
+export default function Testimony({ data }) {
   return (
-    <div>
-      <Star value={2.7} width={35} height={35} spacing={4}></Star>
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-auto" style={{ marginRight: 70 }}>
+          <div className="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
+            <img
+              src={data.imageUrl}
+              alt="Testimonial"
+              className="position-absolute"
+              style={{ zIndex: 1 }}
+            />
+            <img
+              src={TestimonyAccent}
+              alt="Testimonial frame"
+              className="position-absolute"
+              style={{ margin: `-30px 0 0 -30px`, zIndex: 1 }}
+            />
+          </div>
+        </div>
+        <div className="col-6">
+          <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
+          <Star value={2.7} width={35} height={35} spacing={4}></Star>
+          <h5 className="h2 font-weight-light line-height-2 my-3">
+            {data.content}
+          </h5>
+        </div>
+      </div>
     </div>
   );
 }
