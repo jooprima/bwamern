699cf123adc50290779e6f07ef136393c49d202c
jooprima <jooprima@gmail.com>
2022-02-02 21:29:39 +0700

    styling component testimony

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -19,13 +19,13 @@ export default function Testimony({ data }) {
               src={TestimonyAccent}
               alt="Testimonial frame"
               className="position-absolute"
-              style={{ margin: `-30px 0 0 -30px`, zIndex: 1 }}
+              style={{ margin: `-30px 0 0 -30px` }}
             />
           </div>
         </div>
-        <div className="col-6">
+        <div className="col">
           <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
-          <Star value={2.7} width={35} height={35} spacing={4}></Star>
+          <Star value={data.rate} width={35} height={35} spacing={4}></Star>
           <h5 className="h2 font-weight-light line-height-2 my-3">
             {data.content}
           </h5>
