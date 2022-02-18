e450a2bfff1a80eec8fef814997a56cbaffc5c06
jooprima <jooprima@gmail.com>
2022-02-18 13:32:02 +0700

    element breadcrumb

---
+++ src/elements/Breadcrumb/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import propTypes from "prop-types";
+import Button from "elements/Button";
+import "./index.scss";
+
+export default function Breadcrumb() {
+  return (
+    <nav aria-label="breadcrumb">
+      <ul className={className.join(" ")}>
+        {props.data.map((item, index) => {
+          return (
+            <li
+              key={`breadcrumb-${index}`}
+              className={`breadcrumb-item${
+                index === props.data.length - 1 ? " active" : ""
+              }`}
+            >
+              {index === props.data.length - 1 ? (
+                item.pageTitle
+              ) : (
+                <Button type="link" href={item.pageHref}>
+                  {item.pageTitle}
+                </Button>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+Breadcrumb.propTypes = {
+  data: propTypes.array,
+  className: propTypes.string,
+};
