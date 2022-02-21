2d066ed945704d36443bcf502ee0205aae318388
jooprima <jooprima@gmail.com>
2022-02-21 15:28:34 +0700

    booking form part2

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -64,7 +64,57 @@ export default class BookingForm extends Component {
   }
 
   render() {
-    return <div>BookingForm</div>;
+    const { data } = this.state;
+    const { itemDetails, startBooking } = this.props;
+
+    return (
+      <div className="card bordered" style={{ padding: "60px 80px" }}>
+        <h4 className="mb-3">Start Booking</h4>
+        <h5 className="h2 text-teal mb-4">
+          ${itemDetails.price}{" "}
+          <span className="text-gray-500 font-weight-light">
+            per {itemDetails.unit}
+          </span>
+        </h5>
+
+        <label htmlFor="duration">How long you will stay?</label>
+        <InputNumber
+          max={30}
+          suffix={" night"}
+          isSuffixPlural
+          onChange={this.updateData}
+          name="duration"
+          value={data.duration}
+        />
+
+        <label htmlFor="date">Pick a date</label>
+        <InputDate onChange={this.updateDate} name="date" value={data.date} />
+
+        <h6
+          className="text-gray-500 font-weight-light"
+          style={{ marginBottom: 40 }}
+        >
+          You will pay{" "}
+          <span className="text-gray-900">
+            ${itemDetails.price * data.duration} USD
+          </span>{" "}
+          per{" "}
+          <span className="text-gray-900">
+            {data.duration} {itemDetails.unit}
+          </span>
+        </h6>
+
+        <Button
+          className="btn"
+          hasShadow
+          isPrimary
+          isBlock
+          onclick={startBooking}
+        >
+          Continue to Book
+        </Button>
+      </div>
+    );
   }
 }
 
