755f7a659034c6c754737fa77a4c945c5d907ee1
jooprima <jooprima@gmail.com>
2022-02-23 12:03:47 +0700

    improve part header checkout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
 import Example from "pages/Example";
 import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={LandingPage}></Route>
         <Route exact path="/properties/:id" component={DetailsPage} />
         <Route path="/example" component={Example}></Route>
+        <Route path="/checkout" component={Checkout}></Route>
       </Router>
     </div>
   );
