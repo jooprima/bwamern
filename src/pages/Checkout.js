889a5e7cad72c56b256f62bc5846f3679a543a1a
jooprima <jooprima@gmail.com>
2022-03-01 12:40:31 +0700

    Checkout Page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,12 +1,91 @@
 import React, { Component } from "react";
+import { Fade } from "react-reveal/Fade";
 
 import Header from "parts/Header";
+import Button from "elements/Button";
+import Stepper, {
+  Numbering,
+  Meta,
+  MainContent,
+  Controller,
+} from "elements/Stepper";
+import BookingInformation from "parts/Checkout/BookingInformation";
+import Payment from "parts/Checkout/Payment";
+import Completed from "parts/Checkout/Completed";
+import ItemDetails from "json/itemDetails.json";
 
 export default class Checkout extends Component {
+  state = {
+    data: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      proofPayment: "",
+      bankName: "",
+      bankHolder: "",
+    },
+  };
+
+  onChange = (event) => {
+    this.setState({
+      data: {
+        ...this.state.data,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  componentDidMount() {
+    window.scroll(0, 0);
+  }
+
   render() {
+    const { data } = this.state;
+
+    const checkout = {
+      duration: 3,
+    };
+
+    const steps = {
+      bookingInformation: {
+        title: "Booking Information",
+        description: "Please fill up the blank fields below",
+        content: (
+          <BookingInformation
+            data={data}
+            checkout={checkout}
+            ItemDetails={ItemDetails}
+            onChange={this.onChange}
+          />
+        ),
+      },
+      payment: {
+        title: "Payment",
+        description: "Kindly follow the instruction below",
+        content: (
+          <Payment
+            data={data}
+            ItemDetails={ItemDetails}
+            checkout={checkout}
+            onChange={this.onChange}
+          />
+        ),
+      },
+      completed: {
+        title: "Yayy! Completed",
+        description: null,
+        content: <Completed />,
+      },
+    };
+
     return (
       <>
         <Header isCentered />
+
+        <Stepper steps={steps}>
+          (prevStep,nextStep,CurrentStep,steps) =>{}
+        </Stepper>
       </>
     );
   }
