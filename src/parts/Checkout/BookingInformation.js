1965d41fdffd8af1a83042c38774f210dbe2dd04
jooprima <jooprima@gmail.com>
2022-03-01 12:20:04 +0700

    Completed

--- src/parts/Checkout/BookingInformation.js
+++ src/parts/Checkout/BookingInformation.js
@@ -3,6 +3,8 @@ import { Fade } from "react-reveal/Fade";
 import { InputText } from "elements/Form";
 
 export default function BookingInformation(props) {
+  const { data, ItemDetails, checkout } = props;
+
   return (
     <Fade>
       <div className="container" style={{ marginBottom: 30 }}>
